e9502c68070c2b7c6e79c13ee8435da2a00707cc
Abbie Dallmann <acousticnotepad@gmail.com>
2025-02-19 00:40:29 -0600

    Stopped right before step 62. Right after breaking the entire code :>

--- kilo.c
+++ kilo.c
@@ -3,13 +3,18 @@
 
 
 
-/*** includes  ***/
+/*** includes  ***/     // Up to date as of step 59
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -35,12 +40,20 @@ enum editorKey {
 
 
 
-/*** data  ***/
+/*** data  ***/             //Up to date as of Step 61
+// erow = "editor row"
+typedef struct erow {
+    int size;
+    char *chars;
+}   erow;
+
 struct editorConfig {
     // These integers will allow the user to move the cursors around
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -171,6 +184,32 @@ int getWindowSize (int *rows, int *cols) {
 
 
 
+/*** file i/o ***/      // Up to date as of step 56
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != 1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) 
+            linelen--;
+    
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
+}
+
+
+
+
 /*** append buffer ***/
 struct abuf {
     char *b;
@@ -202,21 +241,27 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab){
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        if (y == E.screenrows / 3) {
-            char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
-            if (welcomelen > E.screencols) welcomelen = E.screencols;
+        if (y >= E.numrows) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+                if (welcomelen > E.screencols) welcomelen = E.screencols;
 
-            // divides the screen width by 2 and then subtracts half of the string's length
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
+                // divides the screen width by 2 and then subtracts half of the string's length
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding) {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--) abAppend(ab, " ", 1);
+                abAppend(ab, welcome, welcomelen);
+            }   else {
                 abAppend(ab, "~", 1);
-                padding--;
             }
-            while (padding--) abAppend(ab, " ", 1);
-            abAppend(ab, welcome, welcomelen);
         }   else {
-            abAppend(ab, "~", 1);
+            int len = E.row.size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row.chars, len);
         }
 
         // The 'K' command is the Erase In Line command
@@ -318,20 +363,24 @@ void editorProcessKeypress() {
 
 
 
-/*** init  ***/
+/*** init  ***/         //Up to date as of Step 58
 // Initializes all the fields in the E struct
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
+    E.row = NULL;
     
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
+    if (argc >= 2) {    
+        editorOpen(argv[1]);
+    }
 
-    char c;
     // While the program hasn't timed out, it reads in characters.
     while (1) {
         editorRefreshScreen();
